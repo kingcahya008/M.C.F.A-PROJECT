c08d3c0c359f1073a6e0540b00490fe05dcc4d2a
kingcahya008 <cahyafathuladzim1933@gmail.com>
2026-06-08 18:04:05 +0700

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,40 @@
+const CACHE = 'mcfa-v2';
+const FILES = [
+  './',
+  './MCFA_PROJECT_fixed-4.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return Promise.allSettled(FILES.map(f => cache.add(f)));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      fetch(e.request.clone()).then(res => {
+        if (res && res.ok) cache.put(e.request, res.clone());
+        return res;
+      }).catch(() =>
+        cache.match(e.request).then(r => r || cache.match('./MCFA_PROJECT_fixed-4.html'))
+      )
+    )
+  );
+});
